a39590ebc831b1c26312d78ab978a57088469beb
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-06 11:30:03 +0200

    izmaiņas

--- connect.php
+++ connect.php
@@ -6,16 +6,30 @@ $config = require("config.php");
 
 $db = new database ($config["database"]);
 
-$post = $db->query("SELECT * FROM post")->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM post")->fetchAll(PDO::FETCH_ASSOC);
 
 
 
+
+
+echo "<h1>blog</h1>";
+if (isset($_GET["search_query"])&& $_GET["search_query"] !=""){
+
+    echo "atgriezt ierakstus";
+
+$posts = $db->query("SELECT * FROM posts WHERE content LIKE '%" . $_GET["search_query"] . "%';")->fetchAll();
+
+}
+echo "<form>";
+echo  "<input name='search_query' />";
+echo "<button>meklēt</button>";
+echo "</form>";
 //dabūt rezultātu
 
 
 //dd($post[0]["content"]);
 echo "<ul>";
-foreach ($post as $posts){
-    echo "<li>" . $posts["content"] . "</li>";
+foreach ($posts as $post){
+    echo "<li>" . $post["content"] . "</li>";
 }
 echo "</ul>";
