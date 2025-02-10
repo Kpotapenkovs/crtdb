bc3837a4229af26f1c70466ba5bec3b11900ed0f
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-10 09:51:08 +0200

    gggg

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,12 +1,16 @@
 <?php 
 
+include "Validator.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors = [];
+
+
 
     $sql = "INSERT INTO posts (content) VALUES ( :content );";
     $params = ["content" => $_POST["content"]];
 
-    if (!isset($_POST["content"]) || strlen($_POST["content"]) == 0 || strlen($_POST["content"]) > 50){
+    if (!Validator::string($_POST["content"], max: 50)){
         $errors["content"] = "Saturam jābūt ievadītam, bet ne garākam par 50 rakstzīmēm";
     } else {
         $posts = $db->query($sql, $params)->fetchAll();
